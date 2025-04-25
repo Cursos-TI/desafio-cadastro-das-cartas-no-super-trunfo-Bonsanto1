777ce15096eb0bf6e69644e30d7428b28360a564
Bonsanto1 <iranbonsanto5@gmail.com>
2025-04-25 02:03:00 +0000

    soma corrigido

---
+++ soma.c
@@ -0,0 +1,35 @@
+{
+
+/*
+Soma(+);
+subtração (-);
+multiplicação (*);
+divisão (/);
+igual(=);
+*/
+
+
+int numero1 , numero2;
+
+int soma, subtração, divisão, multiplicação;
+
+int soma = numero1 + numero2 ;
+
+subtração= numero1 - numero2 ;
+
+divisão = numero1 / numero2 ;
+
+multiplicação = numero1 * numero2;
+
+
+
+printf ("Entre com o numero1: /n");
+scanf ("%d" &numero1);
+printf("Entre com o numero2 :/n");
+scanf("%d" &numero2);
+
+printf("A soma é %d /n"soma);
+printf("A subtração é %d /n" subtração);
+printf("A multiplicação é %d /n"multiplicação);
+printf("A divisão é %d /n"divisão);
+}
